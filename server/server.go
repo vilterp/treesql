501a8c38732308d3ab9feb25c0f28eb568416ef0
Pete Vilter <peter.vilter@socrata.com>
2017-05-09 02:03:50 -0700

    basic WS server
    next... use this throughout the stack.
    
    structured logging & messages everywhere I hope!

--- server/server.go
+++ server/server.go
@@ -4,13 +4,14 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"net"
 
 	"os"
 	"os/signal"
 	"syscall"
 
-	"github.com/hashicorp/yamux"
+	"net/http"
+
+	"github.com/gorilla/websocket"
 	treesql "github.com/vilterp/treesql/package"
 )
 
@@ -22,7 +23,7 @@ func main() {
 	var dataFile = flag.String("data-file", "treesql.data", "data file")
 	flag.Parse()
 
-	// open Sophia storage layer
+	// open storage layer
 	database, err := treesql.Open(*dataFile)
 	if err != nil {
 		log.Fatalln("failed to open database:", err)
@@ -38,25 +39,52 @@ func main() {
 		os.Exit(1) // is 1 the proper exit code for Ctrl-C?
 	}()
 
-	// listen for connections
-	listeningSock, listenErr := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-	if listenErr != nil {
-		log.Fatalln("failed to listen for connections:", listenErr)
+	// set up HTTP server
+	http.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) {
+		resp.Write([]byte("Hello from TreeSQL. The action is at /ws"))
+	})
+	upgrader := websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
 	}
-	log.Printf("listening on port %d\n", *port)
-
-	// accept & handle connections
-	connectionID := 0
-	for {
-		conn, _ := listeningSock.Accept()
-		mx, _ := yamux.Server(conn, nil)
-		connection := &treesql.Connection{
-			ClientConn:  mx,
-			ID:          connectionID,
-			Database:    database,
-			NextQueryId: 0,
+	http.HandleFunc("/ws", func(resp http.ResponseWriter, req *http.Request) {
+		conn, err := upgrader.Upgrade(resp, req, nil)
+		if err != nil {
+			log.Println(err)
+			return
 		}
-		connectionID++
-		go connection.Run()
+		conn.WriteMessage(websocket.TextMessage, []byte("hello on a websocket"))
+		_, data, messageErr := conn.ReadMessage()
+		if messageErr != nil {
+			log.Println("websocket message error:", messageErr)
+		}
+		log.Println("WS message received:", string(data))
+	})
+	log.Println("serving HTTP at", fmt.Sprintf("http://localhost:%d/", *port))
+	listenErr := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
+	if listenErr != nil {
+		log.Fatal("error listening:", listenErr)
 	}
+
+	// // listen for connections
+	// listeningSock, listenErr := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	// if listenErr != nil {
+	// 	log.Fatalln("failed to listen for connections:", listenErr)
+	// }
+	// log.Printf("listening on port %d\n", *port)
+
+	// // accept & handle connections
+	// connectionID := 0
+	// for {
+	// 	conn, _ := listeningSock.Accept()
+	// 	mx, _ := yamux.Server(conn, nil)
+	// 	connection := &treesql.Connection{
+	// 		ClientConn:  mx,
+	// 		ID:          connectionID,
+	// 		Database:    database,
+	// 		NextQueryId: 0,
+	// 	}
+	// 	connectionID++
+	// 	go connection.Run()
+	// }
 }
