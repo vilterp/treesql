0709a3644af0af26c34e90ae0986ca8d8afd5359
Pete Vilter <peter.vilter@socrata.com>
2017-05-22 17:07:37 -0700

    systemd service files for running this on (almost) bare ubuntu

--- webui/src/index.js
+++ webui/src/index.js
@@ -4,7 +4,7 @@ import App from './components/App';
 import TreeSQLClient from './lib/TreeSQLClient';
 import './index.css';
 
-const client = new TreeSQLClient(`ws://localhost:9000/ws`)
+const client = new TreeSQLClient(`ws://${window.location.host}:9000/ws`)
 
 ReactDOM.render(
   <App client={client} />,
