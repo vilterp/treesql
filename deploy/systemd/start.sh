ae8e52d05d80cc36305c562a6eb59d78b37cd252
Pete Vilter <peter.vilter@socrata.com>
2017-05-23 01:52:17 -0700

    tweak deploy scripts

--- deploy/systemd/start.sh
+++
@@ -1,2 +0,0 @@
-ssh -i $TREESQL_DEPLOY_KEY $TREESQL_DEPLOY_HOST 'sudo systemctl stop treesql-server; sudo systemctl start treesql-server'
-ssh -i $TREESQL_DEPLOY_KEY $TREESQL_DEPLOY_HOST 'sudo systemctl stop treesql-console; sudo systemctl start treesql-console'
