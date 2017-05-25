0e884cc669c157dbeb5ce41e591d70fa4bc5a270
Pete Vilter <peter.vilter@socrata.com>
2017-05-24 19:03:26 -0700

    tweak deploy scripts to work independent of user

--- webui/src/index.js
+++ webui/src/index.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import TreeSQLClient from './lib/TreeSQLClient';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import TreeSQLClient from "./lib/TreeSQLClient";
+import "./index.css";
 
-const client = new TreeSQLClient(`ws://${window.location.host}/ws`)
+const client = new TreeSQLClient(`ws://${window.location.host}:9000/ws`);
 
-ReactDOM.render(
-  <App client={client} />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App client={client} />, document.getElementById("root"));
