621400e53e5978cdc3a890d6b662ccd971388153
Pete Vilter <vilterp@cockroachlabs.com>
2018-02-28 03:44:12 -0500

    test regexes and WhitespaceSeq

--- package/parserlib/stdlib_test.go
+++ package/parserlib/stdlib_test.go
@@ -48,6 +48,53 @@ func TestOpt(t *testing.T) {
 	})
 }
 
+func TestRegexes(t *testing.T) {
+	g, err := NewGrammar(map[string]Rule{
+		"int_lit":    SignedIntLit,
+		"str_lit":    StringLit,
+		"ident":      Ident,
+		"whitespace": Whitespace,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(g)
+
+	allShouldSucceed(t, g, []succeedCase{
+		{"int_lit", "0"},
+		{"int_lit", "123"},
+		{"int_lit", "-123"},
+		{"str_lit", `"hello world"`},
+		{"str_lit", `"he said \"hello world\" blerp blerp"`},
+		{"ident", "some_name2"},
+		{"ident", "SomeName"},
+		{"whitespace", " "},
+		{"whitespace", "  "},
+		{"whitespace", "\t"},
+		{"whitespace", "\t\n\t"},
+	})
+}
+
+func TestWhitespaceSeq(t *testing.T) {
+	g, err := NewGrammar(map[string]Rule{
+		"whitespace_seq": WhitespaceSeq([]Rule{
+			&Keyword{"a"},
+			&Keyword{"b"},
+			&Keyword{"c"},
+		}),
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(g)
+
+	allShouldSucceed(t, g, []succeedCase{
+		{"whitespace_seq", "a b c"},
+		{"whitespace_seq", "a    b c"},
+		{"whitespace_seq", "a    b\n\tc"},
+	})
+}
+
 type succeedCase struct {
 	rule  string
 	input string
@@ -56,7 +103,7 @@ type succeedCase struct {
 func allShouldSucceed(t *testing.T, g *Grammar, cases []succeedCase) {
 	for caseIdx, testCase := range cases {
 		if _, err := Parse(g, testCase.rule, testCase.input); err != nil {
-			t.Errorf("case %d: expected success for rule %s; got %v", caseIdx, testCase.rule, err)
+			t.Errorf("case %d: rule=%s, input=%s, err=%v", caseIdx, testCase.rule, testCase.input, err)
 		}
 	}
 }
