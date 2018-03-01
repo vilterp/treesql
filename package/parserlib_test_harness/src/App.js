9b3571ce0f88ff9f831f222fd3e6ac3ed0a04e50
Pete Vilter <vilterp@cockroachlabs.com>
2018-02-28 22:19:23 -0500

    render trace and grammar in UI!

--- package/parserlib_test_harness/src/App.js
+++ package/parserlib_test_harness/src/App.js
@@ -1,18 +1,99 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const INITIAL_QUERY = `MANY blog_posts {
+  id,
+  title,
+  comments: MANY comments {
+    id,
+    body
+  }
+}`;
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      query: INITIAL_QUERY,
+      grammar: null,
+      trace: null,
+    }
+  }
+
+  componentDidMount() {
+    this.fetchGrammar();
+    this.fetchQuery(INITIAL_QUERY);
+  }
+
+  fetchGrammar() {
+    fetch("http://localhost:9999/grammar").then((resp) => {
+      resp.json().then((grammar) => {
+        this.setState({
+          grammar: grammar,
+        })
+      }).catch((err) => {
+        console.error("error parsing grammar:", err);
+      });
+    });
+  }
+
+  fetchQuery(query) {
+    const completionReq = {
+      Input: query,
+      CursorPos: 0,
+    };
+    fetch("http://localhost:9999/completions", {
+      method: "POST",
+      body: JSON.stringify(completionReq),
+    }).then((resp) => {
+      resp.json().then((completionResp) => {
+        this.setState({
+          trace: completionResp,
+        });
+      }).catch((err) => {
+        console.error("error parsing completions:", err);
+      });
+    });
+  }
+
+  handleQueryUpdate(val) {
+    this.setState({
+      query: val,
+    });
+    this.fetchQuery(val);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>TreeSQL Test Harness</h1>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+              <textarea
+                cols={80}
+                rows={10}
+                value={this.state.query}
+                onChange={(evt) => this.handleQueryUpdate(evt.target.value)}
+              />
+              </td>
+              <td>
+                <pre>{this.state.query}</pre>
+              </td>
+            </tr>
+            <tr style={{ verticalAlign: "top" }}>
+              <td>
+                <h3>Trace</h3>
+                <pre>{JSON.stringify(this.state.trace, null, 2)}</pre>
+              </td>
+              <td>
+                <h3>Grammar</h3>
+                <pre>{JSON.stringify(this.state.grammar, null, 2)}</pre>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
