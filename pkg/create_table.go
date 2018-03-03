d5c899fce54dccd471040adac91a0f3d2b1acfbe
Pete Vilter <vilterp@cockroachlabs.com>
2018-03-03 18:42:44 -0500

    create a bucket for each `referencestable` column as well as the primary

--- pkg/create_table.go
+++ pkg/create_table.go
@@ -47,62 +47,47 @@ func (db *Database) validateCreateTable(create *CreateTable) error {
 }
 
 func (conn *Connection) ExecuteCreateTable(create *CreateTable, channel *Channel) error {
-	// find primary key
-	var primaryKey string
-	for _, column := range create.Columns {
-		if column.PrimaryKey {
-			primaryKey = column.Name
-			break
-		}
-	}
+	tableDesc := conn.Database.buildTableDescriptor(create)
+	tableRecord := tableDesc.ToRecord(conn.Database)
 	columnRecords := make([]*Record, len(create.Columns))
 	updateErr := conn.Database.BoltDB.Update(func(tx *bolt.Tx) error {
-		tableSpec := conn.Database.AddTable(create.Name, primaryKey, make([]*ColumnDescriptor, len(create.Columns)))
+		// TODO: give ids to tables; create bucket from that
 		// create bucket for new table
-		tx.CreateBucket([]byte(create.Name))
-		// add to in-memory schema
+		tableBucket, err := tx.CreateBucket([]byte(create.Name))
+		if err != nil {
+			return err
+		}
+		// create a bucket for each index
+		// primary key, and each column that references another table
+		for _, col := range tableDesc.Columns {
+			if col.ReferencesColumn != nil || tableDesc.PrimaryKey == col.Name {
+				// TODO: factor this out to an encoding file
+				colIDBytes := make([]byte, 4)
+				binary.BigEndian.PutUint32(colIDBytes, uint32(col.ID))
+				_, err := tableBucket.CreateBucket(colIDBytes)
+				if err != nil {
+					return err
+				}
+			}
+		}
 		// write record to __tables__
 		tablesBucket := tx.Bucket([]byte("__tables__"))
-		tableRecord := tableSpec.ToRecord(conn.Database)
 		tablePutErr := tablesBucket.Put([]byte(create.Name), tableRecord.ToBytes())
 		if tablePutErr != nil {
 			return tablePutErr
 		}
-		// write to __columns__
-		for idx, parsedColumn := range create.Columns {
-			// extract reference
-			var reference *ColumnReference
-			if parsedColumn.References != nil {
-				reference = &ColumnReference{
-					TableName: *parsedColumn.References,
-				}
-			}
-			// build column spec
-			columnSpec := &ColumnDescriptor{
-				ID:               conn.Database.Schema.NextColumnID,
-				Name:             parsedColumn.Name,
-				ReferencesColumn: reference,
-				Type:             NameToType[parsedColumn.TypeName],
-			}
-			conn.Database.Schema.NextColumnID++
-			// put column spec in in-memory schema copy
-			// TODO: synchronize access to this mutable shared data structure!!
-			tableSpec.Columns[idx] = columnSpec
+		// write columns to __columns__
+		for idx, columnDesc := range tableDesc.Columns {
 			// write record to __columns__
-			columnRecord := columnSpec.ToRecord(create.Name, conn.Database)
+			columnRecord := columnDesc.ToRecord(create.Name, conn.Database)
 			columnsBucket := tx.Bucket([]byte("__columns__"))
-			key := []byte(fmt.Sprintf("%d", columnSpec.ID))
+			key := []byte(fmt.Sprintf("%d", columnDesc.ID))
 			value := columnRecord.ToBytes()
 			columnPutErr := columnsBucket.Put(key, value)
 			if columnPutErr != nil {
 				return columnPutErr
 			}
 			columnRecords[idx] = columnRecord
-		}
-		// push live query messages
-		conn.Database.PushTableEvent(channel, "__tables__", nil, tableRecord)
-		for _, columnRecord := range columnRecords {
-			conn.Database.PushTableEvent(channel, "__columns__", nil, columnRecord)
 		}
 		// write next column id sequence
 		nextColumnIDBytes := make([]byte, 4)
@@ -112,6 +97,13 @@ func (conn *Connection) ExecuteCreateTable(create *CreateTable, channel *Channel
 	})
 	if updateErr != nil {
 		return errors.Wrap(updateErr, "creating table")
+	}
+	// add to in-memory schema
+	conn.Database.addTableDescriptor(tableDesc)
+	// push live query messages
+	conn.Database.PushTableEvent(channel, "__tables__", nil, tableRecord)
+	for _, columnRecord := range columnRecords {
+		conn.Database.PushTableEvent(channel, "__columns__", nil, columnRecord)
 	}
 	clog.Println(channel, "created table", create.Name)
 	channel.WriteAckMessage("CREATE TABLE")
