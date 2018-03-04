6810a29612a1a11eb1e8de3c04520b8c0265f602
Pete Vilter <vilterp@cockroachlabs.com>
2018-03-04 03:01:13 -0500

    flatten things out with the indent buffer, but var names are still messed up
    Not sure how far to go with this

---
+++ package/util/indent_buffer.go
@@ -0,0 +1,39 @@
+package util
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
+
+type IndentBuffer struct {
+	depth  int
+	buf    *bytes.Buffer
+	indent string
+}
+
+func NewIndentBuffer(indent string) *IndentBuffer {
+	return &IndentBuffer{
+		depth:  0,
+		buf:    bytes.NewBufferString(""),
+		indent: indent,
+	}
+}
+
+func (ib *IndentBuffer) String() string {
+	return ib.buf.String()
+}
+
+func (ib *IndentBuffer) Indent() {
+	ib.depth++
+}
+
+func (ib *IndentBuffer) Dedent() {
+	ib.depth--
+}
+
+func (ib *IndentBuffer) Printlnf(format string, params ...interface{}) {
+	ib.buf.WriteString(strings.Repeat(ib.indent, ib.depth))
+	ib.buf.WriteString(fmt.Sprintf(format, params...))
+	ib.buf.WriteByte('\n')
+}
