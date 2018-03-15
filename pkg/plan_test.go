af077b8734ccbad893d32e05e8b902ee204a000c
Pete Vilter <vilterp@cockroachlabs.com>
2018-03-15 10:27:38 -0400

    add plan tests

---
+++ pkg/plan_test.go
@@ -0,0 +1,64 @@
+package treesql
+
+import (
+	"testing"
+
+	"github.com/vilterp/treesql/pkg/util"
+)
+
+func TestPlan(t *testing.T) {
+	tsr := runSimpleTestScript(t, []simpleTestStmt{
+		// Create blog post schema.
+		{
+			stmt: `
+				CREATETABLE blog_posts (
+					id string PRIMARYKEY,
+					title string
+				)
+			`,
+			ack: "CREATE TABLE",
+		},
+		{
+			stmt: `
+				CREATETABLE comments (
+					id string PRIMARYKEY,
+					blog_post_id string REFERENCESTABLE blog_posts,
+					body string
+				)
+			`,
+			ack: "CREATE TABLE",
+		},
+	})
+
+	cases := []struct {
+		in  string
+		out string
+		err string
+	}{
+		{
+			`MANY blog_posts { id }`,
+			`map(tables.blog_posts.id.scan, (row) => {
+  id: row.id
+})`,
+			"",
+		},
+	}
+
+	for idx, testCase := range cases {
+		parsedQuery, err := Parse(testCase.in)
+		if err != nil {
+			t.Errorf("parse error: %v", err)
+			continue
+		}
+
+		expr, err := tsr.server.db.schema.planSelect(parsedQuery.Select)
+		if util.AssertError(t, idx, testCase.err, err) {
+			continue
+		}
+
+		formatted := expr.Format().String()
+		if formatted != testCase.out {
+			t.Errorf("expected:\n\n%s\n\ngot:\n\n%s\n", testCase.out, formatted)
+		}
+	}
+}
